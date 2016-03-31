713aad44d48ac8e5179dd8ccae4dbaf172ccc6e0
Jordan Miller <jmiller@mobiquityinc.com>
2016-03-31 16:03:16 -0400

    get things working properly...

--- src/ios/BlurPrivacyScreen.m
+++ src/ios/BlurPrivacyScreen.m
@@ -7,9 +7,10 @@
 @implementation AppDelegate (BlurAppPrivacyScreen)
 
 - (void)applicationWillResignActive:(UIApplication *)application {
+    NSLog(@"Showing blur");
     self.window.backgroundColor = [UIColor clearColor];
 
-    UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleExtraLight];
+    UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
     UIVisualEffectView *blurEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
     blurEffectView.frame = self.window.bounds;
     blurEffectView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
@@ -26,6 +27,20 @@
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
+    NSLog(@"Hiding blur");
+    // grab a reference to our custom blur view
+    UIView *blurEffectView = [self.window viewWithTag:1234];
+
+    // fade away colour view from main view
+    [UIView animateWithDuration:0.5 animations:^{
+        blurEffectView.alpha = 0;
+    } completion:^(BOOL finished) {
+        // remove when finished fading
+        [blurEffectView removeFromSuperview];
+    }];
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
     NSLog(@"Hiding blur");
     // grab a reference to our custom blur view
     UIView *blurEffectView = [self.window viewWithTag:1234];
